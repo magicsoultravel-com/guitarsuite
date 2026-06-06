c590db7b8f0b0dc845cfe427ad916a1158b19495
magicsoultravel-com <camouflagedtiger@proton.me>
2026-06-06 16:04:01 +0200

    kjl

--- inc/google-scripts.php
+++
@@ -1,8 +0,0 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-SLPENRVFDD"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'G-SLPENRVFDD');
-</script>
-<script src="https://apis.google.com/js/platform.js"></script>
